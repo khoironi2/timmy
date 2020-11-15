ed28d2de23b25037f17f70f344ea50ffaa41af3e
Abdul Rosyid <abdulrosyid@localhost.localdomain>
2020-11-14 19:42:56 -0600

    Setup

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,14 +6,8 @@ class Home extends CI_Controller
     {
         $data = [
             'title' => 'Sistem Informasi Bank Sampah Enviro 18',
-            'abouts' => $this->db->get('tbl_about')->result_array()
         ];
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/header_mobile');
-        $this->load->view('templates/sidebar');
-        $this->load->view('templates/topbar_home');
-        $this->load->view('pages/home');
-        $this->load->view('templates/footer');
+        echo "OK";
     }
 }
