eaae099962ea5f5979af214822e1e058336abe04
Abdul Rosyid <abdulrosyid@localhost.localdomain>
2020-11-18 11:38:25 -0600

    Admin => Groming

--- application/views/admin/pets_care/groming/index.php
+++ application/views/admin/pets_care/groming/index.php
@@ -10,29 +10,61 @@
                     <div><?= $halaman; ?></div>
                 </div>
                 <div class="page-title-actions">
-
                     <div class="d-inline-block dropdown">
-                        <a href="" class="btn-shadow btn btn-info">
+                        <a href="<?= base_url('admin/pets_care/groming/create'); ?>" class="btn-shadow btn btn-info">
                             <span class="btn-icon-wrapper pr-2 opacity-7">
                                 <i class="fas fa-business-time fa-w-20"></i>
                             </span>
-                            Buttons
+                            Create
                         </a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 col-xl-4">
-                <div class="card mb-3 widget-content bg-midnight-bloom">
-                    <div class="widget-content-wrapper text-white">
-                        <div class="widget-content-left">
-                            <div class="widget-heading">Total Orders</div>
-                            <div class="widget-subheading">Last year expenses</div>
-                        </div>
-                        <div class="widget-content-right">
-                            <div class="widget-numbers text-white"><span>1896</span></div>
-                        </div>
+            <div class="col-md-12">
+                <?= $this->session->flashdata('message'); ?>
+                <div class="card">
+                    <div class="card-header">
+                        Info Groming
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped" id="datatable">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Pasien</th>
+                                    <th scope="col">Hewan</th>
+                                    <th scope="col">Paket</th>
+                                    <th scope="col">Dijemput</th>
+                                    <th scope="col">Keterangan</th>
+                                    <th scope="col">Harga</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $no = 1; ?>
+                                <?php foreach ($groming as $data) : ?>
+                                    <tr>
+                                        <th scope="row"><?= $no++; ?></th>
+                                        <td><?= $data['name']; ?></td>
+                                        <td><?= $data['nama_hewan_groming']; ?></td>
+                                        <td><?= $data['nama_paket_groming']; ?></td>
+                                        <?php if ($data['dijemput'] == 'ya') : ?>
+                                            <td><span class="badge badge-success"><?= $data['dijemput']; ?></span></td>
+                                        <?php else : ?>
+                                            <td><span class="badge badge-danger"><?= $data['dijemput']; ?></span></td>
+                                        <?php endif; ?>
+                                        <td><?= $data['keterangan_tambahan_groming']; ?></td>
+                                        <td><?= $data['total_harga_groming']; ?></td>
+                                        <td>
+                                            <a href="<?= base_url('admin/pets_care/groming/update/' . $data['id_boking_groming']); ?>" class="btn btn-info btn-sm"><i class="fas fa-edit"></i></a>
+                                            <a onclick="return confirm('Data groming akan terhapus.');" href="<?= base_url('admin/pets_care/groming/destroy/' . $data['id_boking_groming']); ?>" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
