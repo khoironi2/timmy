8d926e66460c603338727ac2d4020c4a39e2123a
Abdul Rosyid <abdulrosyid@localhost.localdomain>
2020-11-19 07:15:09 -0600

    Landing Page

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,6 +6,7 @@ class Home extends CI_Controller
     {
         $data = [
             'title' => 'Sistem informasi klinik pelayanan hewan',
+            'users' => $this->db->get_where('tbl_users', ['level' => 'dokter'], 3)->result_array()
         ];
 
         $this->load->view('layouts/header', $data);
