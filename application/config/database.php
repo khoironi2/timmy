1d6e39ccf226c21ff42da322748c39dd763d2191
khoironi2 <khoironi7887@students.amikom.ac.id>
2020-12-09 10:25:16 +0700

    config done

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost:3306',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'root',
 	'database' => 'sikph',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
